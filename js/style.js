f969a2f41bbe92ebf61691bfff7e91fe4f612088
audrey.huguenin <audrey.huguenin@heig-vd.ch>
2019-01-15 11:11:16 +0100

    vector + interactions done

--- js/style.js
+++ js/style.js
@@ -1,5 +1,5 @@
 function instSportStyle(feature, resolution)
-                 {   var color = feature.get('sport')
+                 {   
                     var instSport = new ol.style.Style({
                         image: new ol.style.Circle({
                             radius: 9,
@@ -15,6 +15,145 @@ function instSportStyle(feature, resolution)
 return instSport;
                     }
 
+function cyclevasionStyle(feature, resolution)
+
+                   {   var iti = feature.get('itineraire');
+
+                    var itLocal102 = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#dff442',//vert clair
+                                width: 3
+                            }),
+                                       
+                    });
+
+                    var itLocal103 = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#f45b41',//rouge
+                                width: 3
+                            }),
+                                       
+                    });
+                    var itLocal104 = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#f4d941',//jaune
+                                width: 3
+                            }),
+                                       
+                    });
+                    var itRegion50 = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#00ce94',//turquoise
+                                width: 4
+                            }),
+                                       
+                    });
+                    var itRegion46 = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#004ece',//bleu
+                                width: 4
+                            }),
+                                       
+                    });
+
+                    var itNation = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: 'rgba(138, 63, 191, 0.5)',//violet
+                                width: 5
+                            }),
+                                       
+                    });
+
+                    switch (iti) {
+                        case "102":
+                          return itLocal102
+                         
+                        case "103":
+                        return itLocal103
+                           
+                        case "104":
+                        return itLocal104
+                          
+                        case "50":
+                        return itRegion50
+                          
+                        case "46":
+                        return itRegion46
+                                                  
+                        default: 
+                          return itNation
+                      }
+                    }
+
+function loisirStyle(feature, resolution)
+
+                   {   var type = feature.get('type_loisi');
+
+                    var cheval = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#9b6331',//brun
+                                width: 4
+                            }),
+                                       
+                    });
+
+                    var mobRed = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#fffb28',//jaune
+                                width: 4
+                            }),
+                                       
+                    });
+                    var sentierTheme = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#61ff28',//vert
+                                width: 4
+                            }),
+                                       
+                    });
+                    var Libellul = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#1fba9d',//turquoise
+                                width: 4
+                            }),
+                                       
+                    });
+                    var flowerWalk = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#f796e4',//rose
+                                width: 4
+                            }),
+                                       
+                    });
+
+                    var pecheMouche = new ol.style.Style({
+                        stroke: new ol.style.Stroke({                            
+                                color: '#1d1df9',//bleu
+                                width: 4
+                            }),
+                                       
+                    });
+
+                    switch (type) {
+                        case "sentiers à thèmes":
+                          return sentierTheme
+                         
+                        case "mobilité réduite":
+                        return mobRed
+                           
+                        case "Libellul'ID":
+                        return Libellul
+                          
+                        case "flower walk":
+                        return flowerWalk
+                          
+                        case "pêche à la mouche":
+                        return pecheMouche
+                                                  
+                        default: 
+                          return cheval
+                      }
+                    }
 
 function selectedStyle(feature, resolution)
                  {   
@@ -45,9 +184,9 @@ function selectedStyle(feature, resolution)
                     });
 
                     var selectStyleLoisir = new ol.style.Style({
-                        image: new ol.style.Stroke({
-                                color: '#FFFF00',
-                                width: 3
+                        stroke: new ol.style.Stroke({
+                                color: '#ff0000',
+                                width: 4
                             }),
                                          
                         });
